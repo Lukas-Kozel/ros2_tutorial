a644b5e4d0ea91c0f775a04ec2e329953c5436b1
LukyKozlojc <l.kozel@outlook.cz>
2023-08-10 20:04:30 +0200

    finished first python node

---
+++ src/my_robot_controller/my_robot_controller/my_first_node.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+#interpreter setup
+
+import rclpy
+from rclpy.node import Node
+
+class MyNode(Node):
+    
+    def __init__(self):
+        super().__init__("first_node")
+        self.counter_ = 0
+        self.create_timer(1.0, self.timer_callback)
+    
+    def timer_callback(self):
+        self.get_logger().info("Hello"+ str(self.counter_))
+        self.counter_+=1
+        
+
+def main(args=None):
+    rclpy.init(args=args) #ros2 initialize communication
+    node = MyNode()  
+    rclpy.spin(node) #this is going to be kept alived until i kill it in terminal
+    rclpy.shutdown() #shutdown of ros2 communication
+
+if __name__ == '__main__':
+    main()
+
+
